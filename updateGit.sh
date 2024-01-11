f1518317eb1e56534540e3b4b4b20bb8e4f49896
orgaworl <orgawrol@outlook.com>
2024-01-11 23:21:31 +0800

    2024-01-11

--- updateGit.sh
+++ updateGit.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+# -------------------------------------
+cat ./updateGit.sh
+git pull
 curDate="$(date --rfc-3339=date)"
 git add .
 git commit -m $curDate
 git push
+# --------------------------------------
